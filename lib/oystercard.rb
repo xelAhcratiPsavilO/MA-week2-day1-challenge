f1c4974a80b245600428d737f4338607f1aaa5b4
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-02-26 15:54:07 +0000

    Statisfy user story to deduct money from Oyster

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,19 @@
 class Oystercard
   attr_reader :balance
   BALANCE = 0
+  LIMIT = 90
 
   def initialize(balance = BALANCE)
     @balance = balance
   end
 
   def top_up(added_money)
+    fail "Balance limit #{LIMIT} exceeded" if (@balance + added_money) > LIMIT
     @balance += added_money
+  end
+
+  def deduct(deducted_money)
+    @balance -= deducted_money
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,8 +11,20 @@ describe Oystercard do
   context "Topping up Oystercard" do
     it "should increase the current balance by a specified amount" do
       result = subject.top_up(13)
-      expect(result).to eq subject.balance
+      expect(result).to eq 13
+    end
+    it "raises an error if attempt to top up would exceed maximum balance" do
+      expect { subject.top_up(91) }.to raise_error "Balance limit #{Oystercard::LIMIT} exceeded"
     end
   end
+
+  context "Deducting from Oystercard" do
+    it "should decrease the current balance by a specified amount" do
+      subject.top_up(24)
+      result = subject.deduct(13)
+      expect(result).to eq 11
+    end
+  end
+
 
 end
