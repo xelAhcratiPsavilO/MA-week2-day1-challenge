94244cb862a4ff69021d3d1f14cf75c3d5c6e568
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-02-27 12:43:30 +0000

    Privatize method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,8 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :card_status
   BALANCE = 0
-  LIMIT = 90
+  TOP_UP_LIMIT = 90
+  ENTRY_LIMIT = 1
 
   def initialize(balance = BALANCE)
     @balance = balance
@@ -9,12 +10,8 @@ class Oystercard
   end
 
   def top_up(added_money)
-    fail "Balance limit #{LIMIT} exceeded" if (@balance + added_money) > LIMIT
+    fail "Exceeded #{TOP_UP_LIMIT} limit" if (@balance + added_money) > TOP_UP_LIMIT
     @balance += added_money
-  end
-
-  def deduct(deducted_money)
-    @balance -= deducted_money
   end
 
   def in_journey?
@@ -22,11 +19,23 @@ class Oystercard
   end
 
  def touch_in
+     fail "Insufficient funds, top up." if check_limit
      @card_status = :in_use
+ end
+
+ def check_limit
+   @balance < ENTRY_LIMIT
  end
 
  def touch_out
    @card_status = :not_in_use
+   deduct(1)
+ end
+
+ private
+
+ def deduct(deducted_money)
+   @balance -= deducted_money
  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,19 +10,18 @@ describe Oystercard do
 
   context "Topping up Oystercard" do
     it "should increase the current balance by a specified amount" do
-      result = subject.top_up(13)
-      expect(result).to eq 13
+      expect(subject.top_up(13)).to eq 13
     end
     it "raises an error if attempt to top up would exceed maximum balance" do
-      expect { subject.top_up(91) }.to raise_error "Balance limit #{Oystercard::LIMIT} exceeded"
+      expect { subject.top_up(91) }.to raise_error "Exceeded #{Oystercard::TOP_UP_LIMIT} limit"
     end
   end
 
   context "Deducting from Oystercard" do
     it "should decrease the current balance by a specified amount" do
-      subject.top_up(24)
-      result = subject.deduct(13)
-      expect(result).to eq 11
+      subject.top_up(1)
+      subject.touch_in
+      expect(subject.touch_out).to eq 0
     end
   end
 
@@ -31,10 +30,32 @@ describe Oystercard do
       expect(subject).not_to be_in_journey
     end
     it "expects to be in a journey after a touch in" do
-      expect(subject.touch_in).to be :in_use
+      subject.top_up(10)
+      subject.touch_in
+      expect(subject.card_status).to be :in_use
+    end
+    context "Trying to start a journey with balance under the limit" do
+      it "returns an error" do
+        subject.top_up(10)
+        10.times do
+          subject.touch_in
+          subject.touch_out
+        end
+        expect { subject.touch_in }.to raise_error "Insufficient funds, top up."
+      end
     end
     it "expects to not to be in a journey after a touch out" do
-      expect(subject.touch_out).to be :not_in_use
+      subject.top_up(10)
+      subject.touch_in
+      subject.touch_out
+      expect(subject.card_status).to be :not_in_use
+    end
+    context "When finishing the journey" do
+      it "updates the balance by deducting the min fare" do
+        subject.top_up(10)
+        subject.touch_in
+        expect { subject.touch_out }.to change{subject.balance}.by(-1)
+      end
     end
   end
 
